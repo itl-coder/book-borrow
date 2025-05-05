0b11f750a88dfa3a47480d06e386473953df1c51
user-system <ituninapp@gmail.com>
2025-05-05 18:07:44 +0800

    feat: 座位样式调整

--- bookborrow-ui/src/views/front/readingroom/ReadingRoomSeat.vue
+++ bookborrow-ui/src/views/front/readingroom/ReadingRoomSeat.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="room-container">
-    <el-card class="room-card">
+    <el-card class="room-card" shadow="hover">
       <div slot="header">
         <span>{{ room.name }}</span>
         <el-button
